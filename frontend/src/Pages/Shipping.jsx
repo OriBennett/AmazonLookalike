a2b859f18a7e0be40dc7503bb7b2893daef901d2
Ori Bennett <oribennett@gmail.com>
2024-01-30 15:01:06 +0200

    steps

--- frontend/src/Pages/Shipping.jsx
+++ frontend/src/Pages/Shipping.jsx
@@ -1,14 +1,32 @@
-import { useState } from "react"
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Store } from "../Store";
+import Title from "../Components/Shared/Title";
+import CheckoutSteps from "../Components/Shared/CheckoutSteps";
 
 const Shipping = () => {
-    const [fullName, setFullName] = useState()
-    const [address, setAddress] = useState()
-    const [city, setCity] = useState()
-    const [postalCode, setPostalCode] = useState()
-    const [country, setCountry] = useState()
-  return (
-    <div>Shipping</div>
-  )
-}
+  const navigate = useNavigate();
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    userInfo,
+    cart: { cartItems },
+  } = state;
+  useEffect(() => {
+    if (cartItems.length === 0) {
+      navigate("/");
+    }
+    if (!userInfo) {
+      navigate("/signin?redirect=/shipping");
+    }
+  }, [cartItems.length, navigate, userInfo]);
 
-export default Shipping
+  return (
+    <div>
+      <Title title="Shipping Details"></Title>
+      <CheckoutSteps step1 step2/>
+
+    </div>
+  );
+};
+
+export default Shipping;
