99fbaf4890367ace4044f72beedc7a6443fcf784
Ori Bennett <oribennett@gmail.com>
2024-01-30 15:11:39 +0200

    built form

--- frontend/src/Pages/Shipping.jsx
+++ frontend/src/Pages/Shipping.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { Store } from "../Store";
 import Title from "../Components/Shared/Title";
 import CheckoutSteps from "../Components/Shared/CheckoutSteps";
+import { Button, Container, Form } from "../imports";
 
 const Shipping = () => {
   const navigate = useNavigate();
@@ -23,8 +24,35 @@ const Shipping = () => {
   return (
     <div>
       <Title title="Shipping Details"></Title>
-      <CheckoutSteps step1 step2/>
-
+      <CheckoutSteps step1 step2 />
+      <Container className="small-container">
+        <h1>Shipping Address</h1>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label>Full Name:</Form.Label>
+            <Form.Control name="fullName" required></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Address:</Form.Label>
+            <Form.Control name="address" required></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>City:</Form.Label>
+            <Form.Control name="city" required></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Postal Code:</Form.Label>
+            <Form.Control name="postalCode" required></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Country:</Form.Label>
+            <Form.Control name="country" required></Form.Control>
+          </Form.Group>
+          <div className="mb-3">
+            <Button variant="primary">Continue</Button>
+          </div>
+        </Form>
+      </Container>
     </div>
   );
 };
