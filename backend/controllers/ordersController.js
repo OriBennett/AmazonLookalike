9ead802b3c8b2dd2fadacd87b4c0afc57f51315e
Ori <OriBennett@gmail.com>
2024-02-01 15:39:34 +0200

    fixed

--- backend/controllers/ordersController.js
+++ backend/controllers/ordersController.js
@@ -1,3 +1,5 @@
+import Order from "../models/Order.js";
+
 const addOrder = async (req, res) => {
     const newOrder = new Order({
         orderItems: req.body.orderItems.map((item) => ({ ...item, product: item._id })),
@@ -9,4 +11,8 @@ const addOrder = async (req, res) => {
         totalPrice: req.body.totalPrice,
         user: req.user._id
     })
+    const order = await newOrder.save()
+    res.status(201).send({ message: 'Order added successfully' }, order);
 };
+
+export { addOrder };
--- backend/routes/ordersRouter.js
+++ backend/routes/ordersRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import { isAuth } from "../utils.js";
+import { addOrder } from "../controllers/ordersController.js";
 
 
 const ordersRouter = express.Router();
