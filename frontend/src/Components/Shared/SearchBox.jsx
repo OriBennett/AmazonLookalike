943a6c84899668ecc9064dc708f35eace1d0ecef
Ori Bennett <oribennett@gmail.com>
2024-02-06 10:12:23 +0200

    Search

--- frontend/src/Components/Shared/SearchBox.jsx
+++ frontend/src/Components/Shared/SearchBox.jsx
@@ -2,19 +2,40 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { getFilterURI } from "../../utils";
 
 const SearchBox = () => {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const {search} = useLocation();
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    const filterURI = getFilterURI(search, {query: query});
+    navigate(filterURI);
+  }, [query]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const filterURI = getFilterURI(search, {query: query});
+    navigate(filterURI);
+  };
+
   return (
-    <Form className="d-flex me-auto w-50">
+    <Form className="d-flex me-auto w-50" onSubmit={submitHandler}>
       <InputGroup>
         <FormControl
+          onChange={(e) => setQuery(e.target.value)}
           type="text"
           name="q"
           id="q"
           placeholder="Search for products"
           aria-describedby="button-search"
         ></FormControl>
-        <Button variant="outline-primary" id="button-search">
+        <Button variant="outline-primary" id="button-search" type="submit">
           <i className="fa fa-search"></i>
         </Button>
       </InputGroup>
--- frontend/src/utils.jsx
+++ frontend/src/utils.jsx
@@ -28,4 +28,25 @@ const addToCartHandler = async (product, cartItems, ctxDispatch) => {
   return;
 };
 
-export { getError, addToCartHandler };
+const getFilterURI = (searchFromURI, filter, skipPathName) => {
+  const searchParams = new URLSearchParams(searchFromURI);
+  const category = searchParams.get('category') || 'all';
+  const query = searchParams.get('query') || 'all';
+  const price = searchParams.get('price') || 'all';
+  const rating = searchParams.get('rating') || 'all';
+  const order = searchParams.get('order') || 'newest';
+  const page = searchParams.get('page') || 1;
+
+  const filterCategory = filter.category || category;
+  const filterQuery = filter.query || query;
+  const filterPrice = filter.price || price;
+  const filterRating = filter.rating || rating;
+  const filterOrder = filter.order || order;
+  const filterPage = filter.page || page;
+
+  const link = `${skipPathName? "": "/search?"}category=${filterCategory}&query="${filterQuery}"&price=${filterPrice}&rating=${filterRating}&order=${filterOrder}&page=${filterPage}`
+
+  return link;
+};
+
+export { getError, addToCartHandler, getFilterURI };
